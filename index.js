2c4fb34ab00b1ef236385c09af54f682423c12f3
Irla Andrade <paula.irla@gmail.com>
2020-09-21 22:32:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+});
+
+// Endpoints de envio de mensagens
+// CRUD -> Create, Read (Read All e Read Single), Update and Delete
+// CRUD Criar, Ler (Ler tudo e ler individualmente), Atualizar e Remover
+
+//Read All
+app.get('/mensagens', (req, res) => {
+    res.send('Exibir toda a lista de mensagens.');
+});
+
+// Create
+app.post('/mensagens', (req, res) => {
+    res.send('Criar uma mensagem.');
+});
+
+// Read Single
+app.get('/mensagens/:id', (req, res) => {
+    res.send('Exibe uma mensagem selecionada pelo ID que foi informado.');
+});
+
+// Update
+app.put('/mensagens/:id', (req, res) => {
+    res.send('Atualiza uma mensagem selecionada pelo ID que foi informado.');
+});
+
+// Delete
+app.delete('/mensagens/:id', (req, res) => {
+    res.send('Remove uma mensagem selecionada pelo ID que foi informado.');
+});
+
+app.listen(port, () => {
+    console.log(`App rodando em http://localhost:${port}`);
+});
